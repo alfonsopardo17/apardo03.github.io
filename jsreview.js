409d749f8bcfb129cae7012007880df311b131ee
alfonsopardo17 <alfonsospardo101@gmail.com>
2025-07-10 14:11:50 -0400

    Update jsreview.js

--- jsreview.js
+++ jsreview.js
@@ -1,17 +1,14 @@
-var stemclass = "technoligy";
-let myName = "Alfonso"
+var stemclass = "Technology";
+let myName = "Alfonso";
 
-console.log(`This code was writen in the STEM PREP ${stemclass} class`);
+console.log(`This code was written in the STEM PREP ${stemclass} class`);
 
-alert(`This code was writen in the STEM PREP ${stemclass} class by ${myName}`);
+alert(`This code was written in the STEM PREP ${stemclass} class by ${myName}`);
 
 var name = prompt("Hello, what is your name?");
-var school = prompt("What school do you attend?");
-var grade = prompt("What grade are you in?");
+var school = prompt("What school do you attend? ");
+var grade = prompt("What grade are you in? ");
 
 document.getElementById("name").innerHTML = "Name: <b>" + name + "</b>";
-document.getElementById("school").innerHTML = "Name <b>" + name "</b>";
-document.getElementById("grade").innerHTML = "Name <b>" + name "</b>";
-
-
-
+document.getElementById("school").innerHTML = "School: <b>" + school + "</b>";
+document.getElementById("grade").innerHTML = "Grade: <b>" + grade + "</b>";

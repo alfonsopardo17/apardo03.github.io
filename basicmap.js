b6eb9c65eed345cd1f2938c0cd3447ad625a58a0
alfonsopardo17 <alfonsospardo101@gmail.com>
2025-07-15 14:14:41 -0400

    Create basicmap.js

---
+++ basicmap.js
@@ -0,0 +1,11 @@
+//35.2176665°N, 80.831473°W -> 35.2176665, -80.831473
+
+var map = L.map("map", {
+  center: [35.2176665, -80.831473],
+  zoom: 10
+});
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);

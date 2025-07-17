7028b598a68e2e88edd75b8d917ee32d1a8ac69e
alfonsopardo17 <alfonsospardo101@gmail.com>
2025-07-17 13:21:58 -0400

    Update basicmap.js

--- basicmap.js
+++ basicmap.js
@@ -10,9 +10,9 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-let centralMarker = L.marker([35.2176665, -80.831473], {title: "CPCC Central Campus"}).addTo(map);
-let levineMarker = L.marker([ 35.106105299999996, -80.69308509999999], {title: "CPCC Levine Campus Location"}).addTo(map);
-let catoMarker = L.marker([35.2658627, -80.73116], {title: "CPCC CATO Campus"}).addTo(map);
-let harperMarker = L.marker([35.12, -80.93], {title: "CPCC Harper Campus"}).addTo(map);
-let harrisMarker = L.marker([35.22, -80.92], {title: "CPCC Harris Campus"}).addTo(map);
-let merancasMarker = L.marker([35.39, -80.84], {title: "CPCC Merancas Campus"}).addTo(map);
+let centralMarker = L.marker([35.2176665, -80.831473], {title: "CPCC Central Campus"}).bindPopup("<p>Central Campus</p>").addTo(map);
+let levineMarker = L.marker([ 35.106105299999996, -80.69308509999999], {title: "CPCC Levine Campus Location"}).bindPopup("<p>Levine Campus</p>").addTo(map);
+let catoMarker = L.marker([35.2658627, -80.73116], {title: "CPCC CATO Campus"}).bindPopup("<p>CATO Campus</p>").addTo(map);
+let harperMarker = L.marker([35.12, -80.93], {title: "CPCC Harper Campus"}).bindPopup("<p>Harper Campus</p>").addTo(map);
+let harrisMarker = L.marker([35.22, -80.92], {title: "CPCC Harris Campus"}).bindPopup("<p>Harris Campus</p>").addTo(map);
+let merancasMarker = L.marker([35.39, -80.84], {title: "CPCC Merancas Campus"}).bindPopup("<p>Merancas Campus</p>").addTo(map);
